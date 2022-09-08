cef07cced18b4bcb067ec976f345401f3623515f
julio cesar <juliocgcj@hotmail.com>
2022-09-08 16:55:14 -0300

    Primeiro exercício de JS

---
+++ exercicio-1.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+const peso = prompt('Digite seu peso: ');
+console.log(`Seu peso é: ${peso}`);
+
+const nome = prompt('Digite seu nome: ');
+console.log(`Seu nome é: ${nome}`);
+
+const altura = prompt('Digite sua altura: ');
+console.log(`Sua altura é: ${altura}`);
+
+const dataInternacao = prompt('Digite sua data de internação: ');
+console.log(`Sua altura é: ${dataInternacao}`);
+
+const diabetes = prompt('Você tem diabetes? ');
+console.log(`Sua resposta: ${diabetes}`);
